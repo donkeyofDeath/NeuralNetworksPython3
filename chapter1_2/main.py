e778471029b5ed1b244d9ba8b66181b71808122e
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-05-18 20:21:52 +0200

    Checked the format of the arrays used in the program.

--- chapter1_2/main.py
+++ chapter1_2/main.py
@@ -7,13 +7,22 @@ sys.path.append("../")
 import mnist_loader
 
 # Load the data for the neural network.
-train_data, validation_data, verification_data = mnist_loader.load_data_wrapper()
+training_data, validation_data, verification_data = mnist_loader.load_data_wrapper()
 
 # Parameters for the neural network.
+# mini_batch_size = 100
 layer_sizes = [784, 30, 10]
 weights = [np.random.randn(y, x) for x, y in zip(layer_sizes[:-1], layer_sizes[1:])]
 biases = [np.random.randn(y, 1) for y in layer_sizes[1:]]
 
 net = nw.Network(layer_sizes, weights, biases)  # Declare the network.
 
-net.learn(train_data, 30, 10, 3.0, test_data=verification_data)  # Let the network learn.
+# mini_batches = [training_data[k:k + mini_batch_size] for k in range(0, len(training_data), mini_batch_size)]
+
+# x, y = mini_batches[0][0]
+
+# delta_b, delta_w = net.backprop(x, y)
+
+# print(delta_w, delta_b)
+
+net.learn(training_data, 30, 10, 3.0, test_data=verification_data)  # Let the network learn.
